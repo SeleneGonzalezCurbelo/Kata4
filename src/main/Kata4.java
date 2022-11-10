67ba455b3269972c45eeba63061fc9b84f556859
selenegc01 <selene.gonzalez106@alu.ulpgc.es>
2022-11-10 22:23:52 +0000

    Segunda versión de kata4

--- src/main/Kata4.java
+++ src/main/Kata4.java
@@ -11,9 +11,18 @@ public class Kata4 {
         
     public static void main(String[] args) {       
         String fileName = "email.txt";
-        List<Mail> mailList = MailListReader.read(fileName); 
-        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
-        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
-        histoDisplay.execute();
+        input(fileName);   
+    }
+    
+    public static void input(String fileName){
+        process(MailListReader.read(fileName));
+    }
+    
+    public static void process(List<Mail> mailList){
+        output(MailHistogramBuilder.build(mailList));
+    }
+    
+    public static void output(Histogram<String> histogram){
+        new HistogramDisplay(histogram).execute();
     }
 }
